e6f23438ccc8a21065da3e201f56e2222f78e1d2
kingyangking <31798353+kingyangking@users.noreply.github.com>
2018-03-28 15:36:42 +0800

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,8 @@
+package chat;
+
+public class Client {
+	public static void main(String[] args) {
+
+	Server_client_Ui client = new Server_client_Ui("clinet", "server", "127.0.0.1", 4009,false);
+	}
+}
