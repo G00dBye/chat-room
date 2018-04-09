b74d0e0b8f351c6942ed43251882f405ea6219cb
kingyangking <31798353+kingyangking@users.noreply.github.com>
2018-04-09 20:18:48 +0800

    Delete Client.java

--- Client.java
+++
@@ -1,8 +0,0 @@
-package chat;
-
-public class Client {
-	public static void main(String[] args) {
-
-	Server_client_Ui client = new Server_client_Ui("clinet", "server", "127.0.0.1", 4009,false);
-	}
-}
